d3ecaadc890553bf829905c7639bd84b6954b020
itsmeow <mods.itsmeow@gmail.com>
2019-03-20 10:32:14 -0400

    Make base mod

---
+++ src/main/java/its_meow/basemod/Ref.java
@@ -0,0 +1,9 @@
+package its_meow.basemod;
+
+public class Ref {
+	
+	public static final String MODID = "basemod";
+	public static final String VERSION = "1.0.0";
+	public static final String NAME = "Basemod";
+	
+}
